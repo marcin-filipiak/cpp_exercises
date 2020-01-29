261bf71ce1dfef2f8a78ed43dd2cf7a59efe9ff0
marcin-filipiak <50583432+marcin-filipiak@users.noreply.github.com>
2020-01-29 21:57:50 +0100

    Add files via upload

---
+++ ascii_tabela/main.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <iostream>
+
+
+
+int main()
+{
+       for(int x=33;x<126;x++){
+       std::cout<<"\n liczba "<<x<<" znak "<<(char)x;
+   }
+    return 0;
+}
