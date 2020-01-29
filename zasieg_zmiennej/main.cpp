eba0ef20cc35ab8f32cc9a33bb1c28ac2b180b6e
marcin-filipiak <50583432+marcin-filipiak@users.noreply.github.com>
2020-01-29 22:00:20 +0100

    Add files via upload

---
+++ zasieg_zmiennej/main.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int x=3;
+
+int hej(){
+    int z=2;
+    return 0;
+}
+
+int main()
+{
+    int z=1;
+    cout<<"to jest x"<<x;
+    return 0;
+}
+
