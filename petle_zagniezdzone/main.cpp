42629ca0991835a8639f1ab1eb6d55f8cc6dee27
marcin-filipiak <50583432+marcin-filipiak@users.noreply.github.com>
2020-01-29 21:56:34 +0100

    Add files via upload

---
+++ petle_zagniezdzone/main.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ //wiersz
+ for (int y=0;y<5;y++){
+    //* w linii
+    for (int x=0;x<y;x++){
+        cout<<"*";
+    }
+    //enter
+    cout<<"\n";
+ }
+
+    return 0;
+}
