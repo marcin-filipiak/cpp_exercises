d507400a2d83098c0f3066b96a042f43af23d258
marcin-filipiak <50583432+marcin-filipiak@users.noreply.github.com>
2020-01-29 21:54:33 +0100

    Add files via upload

---
+++ tablice/main.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char tekst[4];
+    
+    tekst[0]='a';
+    tekst[1]='l';
+    tekst[2]='a';
+    
+    for(int x=0;x<3;x++){
+      cout<<tekst[x];
+    }
+ 
+    return 0;
+}
