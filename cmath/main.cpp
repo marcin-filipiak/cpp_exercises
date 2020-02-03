7a2e74333f37228b73f2f9af186ae33500c3f067
marcin-filipiak <50583432+marcin-filipiak@users.noreply.github.com>
2020-02-03 16:02:47 +0100

    Add files via upload

---
+++ cmath/main.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <ctime>
+
+//dodawanie
+int dodaj(int a, int b){
+    int wynik = a + b;
+    return wynik;
+}
+
+//losowanie z zakresu
+int losowanie(int nmin, int nmax){
+    return rand() % (nmax - nmin + 1) + nmin;
+}
+
+//pierwiastek dowolnego stopnia
+double root(double x, double a){
+    return pow(x, 1/a);
+}
+
+int main()
+{
+    //std::cout<<dodaj(2,1)<<std::endl;
+    //std::cout<<pow(2.23,2)<<std::endl;
+    //std::cout<<sqrt(16)<<std::endl;
+    //std::cout<<root(16,2)<<std::endl;
+    //std::cout<<sin(30)<<std::endl;
+    //std::cout<<(rand()%100+1)<<std::endl;
+    //std::cout<<time(NULL)<<std::endl;
+    srand(time(NULL));
+    std::cout<<losowanie(10, 100)<<std::endl;
+    return 0;
+}
